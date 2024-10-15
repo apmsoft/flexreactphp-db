5536cdc8fe0be4367d4317d2c6115aaf7716ca8f
JongKwan <apmsoft@gmail.com>
2024-10-15 15:24:47 +0900

    - router 및 middleware 단계 기본 설정

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,8 +1,11 @@
 ## -project-name : flexreact-php-mysql
 ## -version-code : 1
-## -version-name : v0.1.0
+## -version-name : v0.1.1
 
 # Changelog
+
+### - 2024-10-15
+- router 및 middleware 단계 기본 설정
 
 ### - 2024-10-13
 - init
--- app/server.php
+++ app/server.php
@@ -1,74 +1,165 @@
 <?php
-use FastRoute\Dispatcher;
-use FastRoute\RouteCollector;
 use React\Http\Message\Response;
 use Psr\Http\Message\ServerRequestInterface;
-use React\Promise\Promise;
-use React\Cache\ArrayCache;
 
 use Flex\Banana\Classes\App;
 use Flex\Banana\Classes\Log;
 use Flex\Banana\Classes\R;
+use Flex\Banana\Classes\Json\JsonEncoder;
 use Flex\Banana\Classes\Date\DateTimez;
 use Flex\Banana\Classes\Db\DbMySqli;
+use Flex\Banana\Utils\Requested;
 
 # autoload
 require __DIR__. '/vendor/autoload.php';
 
-# classes
-App::init();
-R::init(App::$language ?? '');
-
 # config 설정
 Log::init( Log::MESSAGE_ECHO );
 
-# Resource
-R::parser(__DIR__.'/res/values/sysmsg.json', 'sysmsg');
 
+// 허용할 IP 주소 목록
+$allowedIps = ['192.168.65.1']; // 허용 IP 주소
 
-$handler = function (Psr\Http\Message\ServerRequestInterface $request)
+# router
+$browser = new React\Http\Browser();
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) use ($browser)
 {
-    $db = new DbMySqli("flexreact-php-mysql:test_db","test","test!@!@",3306);
-    try {
-        Log::d("request2 -> ", (new DateTimez("now"))->format('Y-m-d H:i:s'));
+    $r->addRoute('GET', '/', function(Requested $requested) use ($browser): string {
+        return  JsonEncoder::toJson( ["result"=>"true","msg"=>"Hello"] );
+    });
+    $r->addRoute('GET', '/favicon.ico', function(Requested $requested) use ($browser): string {
+        return  JsonEncoder::toJson( ["result"=>"true","msg"=>"Hello"] );
+    });
+    $r->addRoute('OPTIONS', '/', function(Requested $requested) use ($browser): string {
+        return  JsonEncoder::toJson( ["result"=>"true","msg"=>"Hello"] );
+    });
 
-        #간단한 쿼리 실행
-        $result = $db->query("SELECT 1 + 1 as result");
-        if ($result) {
-            $row = $result->fetch_assoc();
-            $response = [
-                'status' => 'success',
-                'message' => 'Successfully connected to MySQL 8888',
-                'result' => 'ddd'
-            ];
-            $result->free();
-        } else {
-            throw new Exception('Query failed: ' . $db->error);
+    // $r->addGroup('/v1', function (RouteCollector $r) use ($browser) 
+    // {
+    //     $r->addRoute('POST', '/bbs/notice', function(Requested $requested) use ($browser): string {
+    //         return  JsonEncoder::toJson( ["result"=>"true","msg"=>"Hello"] );
+    //     });
+    // });
+});
+
+# $db = new DbMySqli("flexreact-php-mysql:test_db","test","test!@!@",3306);
+$handler = function (Psr\Http\Message\ServerRequestInterface $request) use ($dispatcher)
+{
+    # 기본정보
+    $requested = new Requested($request);
+
+    # promise
+    $deferred = new \React\Promise\Deferred();
+    $deferred->promise()
+    ->then(function($requested) use ($dispatcher, $deferred){
+        try {
+            $routeInfo = $dispatcher->dispatch($requested->getMethod(), $requested->getUri()->getPath());
+            match($routeInfo[0]) {
+                FastRoute\Dispatcher::NOT_FOUND => throw new \Exception(JsonEncoder::toJson(["result"=>"false","msg_code"=>404,"msg"=>"Not Found"])),
+                FastRoute\Dispatcher::METHOD_NOT_ALLOWED => throw new \Exception(JsonEncoder::toJson(["result"=>"false","msg_code"=>405,"msg"=>"Method Not Allowed"])),
+                FastRoute\Dispatcher::FOUND => (function() use ($requested,$routeInfo,$deferred)
+                {
+                    $handler = $routeInfo[1];
+                    $data = $handler($requested);
+
+                    Log::v($data);
+                    $deferred->resolve(new Response(status: 200, headers: ['Content-Type' => 'application/json'], body: $data));
+                })()
+            };
+
+        # 예외처리
+        } catch (\Exception $e) {
+            Log::e($e->getMessage());
+            $deferred->resolve(new Response(status: 200, headers: ['Content-Type' => 'application/json'], body: $e->getMessage()));
         }
+    })
+    ->otherwise(function ($e) use ($deferred){
+        $err_msg = $e->getMessage();
+        Log::e( $err_msg);
+        $deferred->resolve(  new React\Promise\Promise(function ($resolve) use ($err_msg) {
+            $resolve(new React\Http\Message\Response( status: 200, headers: ['Content-Type' => 'application/json'], body: $err_msg));
+        }));
+    });
 
-        return new React\Http\Message\Response(
-            200, 
-            ['Content-Type' => 'application/json'],
-            json_encode($response)
-        );
-    } catch (Exception $e) {
-        Log::e('Error details: ' . $e->getMessage());
-        Log::e('Stack trace: ' . $e->getTraceAsString());
-        return new React\Http\Message\Response(
-            500,
-            ['Content-Type' => 'application/json'],
-            json_encode([
-                'status' => 'error',
-                'message' => $e->getMessage()
-            ])
-        );
-    }
+    # run
+    $deferred->resolve($requested);
+
+    # return
+    return $deferred->promise();
 };
 
 $http = new React\Http\HttpServer(
     new React\Http\Middleware\StreamingRequestMiddleware(),
     new React\Http\Middleware\RequestBodyBufferMiddleware(20 * 1024 * 1024), // 20 MiB per request
     new React\Http\Middleware\RequestBodyParserMiddleware(),
+
+    # 웹브라우저 허용 URL
+    function (ServerRequestInterface $request, callable $next)
+    {
+        $uri_path  = $request->getUri()->getPath();
+        $method    = $request->getMethod();
+        if($uri_path == '/favicon.ico' || $method == 'OPTIONS')
+        {
+            return new Response( 
+                status: 200, headers: ['Content-Type' => 'application/json'],
+                body: JsonEncoder::toJson(["result"=>"true","msg"=>"test"])
+            );
+        }
+
+        return $next($request);
+    },
+
+    # IP 체크 미들웨어 추가
+    function (ServerRequestInterface $request, callable $next) use ($allowedIps) 
+    {
+        $clientIp = $request->getServerParams()['REMOTE_ADDR'];
+        Log::d('clientIp',$clientIp);
+
+        # reject
+        // if (!in_array($clientIp, $allowedIps)) {
+        //     return new Response(
+        //         status: 403,
+        //         headers: ['Content-Type' => 'application/json'],
+        //         body: JsonEncoder::toJson(['result'=>'false', 'msg'=>'Access Denied'])
+        //     );
+        // }
+
+        return $next($request);
+    },
+
+    # 언어 및 리소스 설정
+    function (ServerRequestInterface $request, callable $next)
+    {
+        # 언어설정
+        $acceptLanguage = $request->getHeaderLine('Accept-Language');
+        $languages = explode(',', $acceptLanguage);
+        $primaryLanguage = substr($languages[0], 0, 2);
+        Log::d('primaryLanguage',$primaryLanguage);
+
+        # 리소스
+        R::init($primaryLanguage ?? '');
+        R::parser(__DIR__.'/res/values/sysmsg.json', 'sysmsg');
+
+        return $next($request);
+    },
+
+    # 접속 토큰
+    function (ServerRequestInterface $request, callable $next)
+    {
+        // try{
+        //     $header_access_token = $request->getHeaderLine('Authorization-Access-Token') ?? 'Nan';
+        //     (new CheckAccessToken( $header_access_token ))->matching();
+        // }catch(\Exception $e){
+        //     Log::e($e->getMessage());
+        //     return new Response(
+        //         status: 403,
+        //         headers: ['Content-Type' => 'application/json'],
+        //         body: JsonEncoder::toJson(['result'=>'false', 'msg'=>$e->getMessage()])
+        //     );
+        // }
+
+        return $next($request);
+    },
     $handler
 );
 
