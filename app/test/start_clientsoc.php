9772e89775eb04c8f662cf0ddd7514fd0cc493a7
JongKwan <apmsoft@gmail.com>
2024-10-28 10:49:42 +0900

    백그라운드 테스트 300개

--- app/test/start_clientsoc.php
+++ app/test/start_clientsoc.php
@@ -129,14 +129,243 @@ exec("php -q test_clientsoc.php 99 >> log_clientsoc.out &");
 exec("php -q test_clientsoc.php 100 >> log_clientsoc.out &");
 
 // 110
-// exec("php -q test_clientsoc.php 101 >> log_clientsoc.out &");
-// exec("php -q test_clientsoc.php 102 >> log_clientsoc.out &");
-// exec("php -q test_clientsoc.php 103 >> log_clientsoc.out &");
-// exec("php -q test_clientsoc.php 104 >> log_clientsoc.out &");
-// exec("php -q test_clientsoc.php 105 >> log_clientsoc.out &");
-// exec("php -q test_clientsoc.php 106 >> log_clientsoc.out &");
-// exec("php -q test_clientsoc.php 107 >> log_clientsoc.out &");
-// exec("php -q test_clientsoc.php 108 >> log_clientsoc.out &");
-// exec("php -q test_clientsoc.php 109 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 101 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 102 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 103 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 104 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 105 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 106 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 107 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 108 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 109 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 110 >> log_clientsoc.out &");
 
-?>
+// 120
+exec("php -q test_clientsoc.php 111 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 112 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 113 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 114 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 115 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 116 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 117 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 118 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 119 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 120 >> log_clientsoc.out &");
+
+// 130
+exec("php -q test_clientsoc.php 121 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 122 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 123 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 124 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 125 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 126 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 127 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 128 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 129 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 130 >> log_clientsoc.out &");
+
+// 140
+exec("php -q test_clientsoc.php 131 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 132 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 133 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 134 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 135 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 136 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 137 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 138 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 139 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 140 >> log_clientsoc.out &");
+
+// 150
+exec("php -q test_clientsoc.php 141 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 142 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 143 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 144 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 145 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 146 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 147 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 148 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 149 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 150 >> log_clientsoc.out &");
+
+// 160
+exec("php -q test_clientsoc.php 151 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 152 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 153 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 154 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 155 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 156 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 157 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 158 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 159 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 160 >> log_clientsoc.out &");
+
+
+// 170
+exec("php -q test_clientsoc.php 161 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 162 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 163 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 164 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 165 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 166 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 167 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 168 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 169 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 170 >> log_clientsoc.out &");
+
+// 180
+exec("php -q test_clientsoc.php 171 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 172 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 173 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 174 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 175 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 176 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 177 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 178 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 179 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 180 >> log_clientsoc.out &");
+
+// 190
+exec("php -q test_clientsoc.php 181 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 182 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 183 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 184 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 185 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 186 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 187 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 188 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 189 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 190 >> log_clientsoc.out &");
+
+
+// 200
+exec("php -q test_clientsoc.php 191 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 192 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 193 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 194 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 195 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 196 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 197 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 198 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 199 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 200 >> log_clientsoc.out &");
+
+// 210
+exec("php -q test_clientsoc.php 201 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 202 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 203 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 204 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 205 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 206 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 207 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 208 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 209 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 210 >> log_clientsoc.out &");
+
+// 220
+exec("php -q test_clientsoc.php 211 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 212 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 213 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 214 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 215 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 216 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 217 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 218 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 219 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 220 >> log_clientsoc.out &");
+
+// 230
+exec("php -q test_clientsoc.php 221 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 222 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 223 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 224 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 225 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 226 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 227 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 228 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 229 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 230 >> log_clientsoc.out &");
+
+// 240
+exec("php -q test_clientsoc.php 231 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 232 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 233 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 234 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 235 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 236 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 237 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 238 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 239 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 240 >> log_clientsoc.out &");
+
+// 250
+exec("php -q test_clientsoc.php 241 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 242 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 243 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 244 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 245 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 246 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 247 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 248 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 249 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 250 >> log_clientsoc.out &");
+
+// 260
+exec("php -q test_clientsoc.php 251 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 252 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 253 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 254 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 255 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 256 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 257 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 258 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 259 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 260 >> log_clientsoc.out &");
+
+// 270
+exec("php -q test_clientsoc.php 261 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 262 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 263 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 264 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 265 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 266 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 267 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 268 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 269 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 270 >> log_clientsoc.out &");
+
+// 280
+exec("php -q test_clientsoc.php 271 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 272 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 273 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 274 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 275 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 276 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 277 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 278 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 279 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 280 >> log_clientsoc.out &");
+
+// 290
+exec("php -q test_clientsoc.php 281 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 282 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 283 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 284 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 285 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 286 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 287 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 288 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 289 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 290 >> log_clientsoc.out &");
+
+// 300
+exec("php -q test_clientsoc.php 291 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 292 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 293 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 294 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 295 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 296 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 297 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 298 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 299 >> log_clientsoc.out &");
+exec("php -q test_clientsoc.php 300 >> log_clientsoc.out &");
